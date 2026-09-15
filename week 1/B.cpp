0c048b2534866c4c413cdcfa879354cbce3bdea2
VeniceJiaozi <2366759931@qq.com>
2026-09-15 20:33:50 +0800

    2026.9.15

---
+++ week 1/B.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int x;
+    cin>>x;
+
+    int y=x*x+2*x-10;
+    cout<<y<<endl;
+    return 0;
+}
